02fee1e8ddcaf1fedb7bf1908f1f7bb0eb37473a
Dimas Adimo <dimasadi1308@gmail.com>
2024-02-27 17:49:10 +0700

    feat: Day 7 - Routing NextJs

--- components/menu/index.js
+++ components/menu/index.js
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 import styles from './styles.module.css';
 
 const Menu = () => {
   return (
     <div className={styles.header}>
-      <a href="/">Home</a>
-      <a href="/profile">Profile</a>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/profile">Profile</Link></li>
+        <li><Link href="/users">User</Link></li>
+      </ul>
     </div>
   )
 }
---
+++ pages/users/index.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout";
+
+export default function Users() {
+  return (
+    <Layout metaTitle="User" metaDescription="All contents belong to User">
+      <p className="background-orange">User</p>
+    </Layout>
+  );
+}
