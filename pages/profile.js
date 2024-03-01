7064fd1336dbc98315d53f9f9a624c7e3637501e
Dimas Adimo <dimasadi1308@gmail.com>
2024-03-01 20:32:31 +0700

    feat: Day 10 - Image & Dynamic NextJs

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,22 @@
-import Layout from "@/layout";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const LayoutComponent = dynamic(() => import('@/layout'), {
+  loading: () => <p>Loading...</p>,
+})
 
 export default function Profile() {
   return (
-    <Layout metaTitle="Profile" metaDescription="All contents belong to Profile">
+    <LayoutComponent metaTitle="Profile" metaDescription="All contents belong to Profile">
       <p className="background-orange">Profile</p>
-    </Layout>
+      <Image
+      src="/dog_cartoon.jpg"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+      blurDataURL="data:..."
+      placeholder="blur"
+    />
+    </LayoutComponent>
   );
 }
